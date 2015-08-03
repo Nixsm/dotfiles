0370b096bc28a5de2b0ccf1014daff691ded9211
Nixsm <nichsmm@gmail.com>
2015-08-02 22:00:18 -0300

    ops

--- .emacs.d/.cask/24.5.1/elpa/mu4e-multi-20141208.2254/mu4e-multi-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; mu4e-multi-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "mu4e-multi" "mu4e-multi.el" (21949 28065 0
-;;;;;;  0))
-;;; Generated autoloads from mu4e-multi.el
-
-(autoload 'mu4e-multi-compose-new "mu4e-multi" "\
-Start writing a new message.
-This is a simple wrapper over `mu4e-compose-new' that asks for an
-account to be used to compose the new message.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; mu4e-multi-autoloads.el ends here
--- .emacs.d/.cask/24.5.1/elpa/mu4e-multi-20141208.2254/mu4e-multi-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "mu4e-multi" "20141208.2254" "Multiple account facilities for mu4e" 'nil)
